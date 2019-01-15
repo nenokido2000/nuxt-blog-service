8b084652ea1ae3e198fc59758936845b17b9705a
Naoki Enokido <nenokido2000@gmail.com>
2019-01-16 01:07:45 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# nuxt-blog-service
+
+> My striking Nuxt.js project
+
+Nuxt.jsビギナーズガイドの写経
+
+## Build Setup
+
+``` bash
+# set your Firebase Database URL
+$ export FIREBASE_URL=https://xxxxx.firebaseio.com
+
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+  srcDir: 'app',
+
+  router: {
+    middleware: ['auth-cookie']
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  env: {
+    FIREBASE_URL: process.env.FIREBASE_URL
+  },
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    baseURL: process.env.FIREBASE_URL
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
